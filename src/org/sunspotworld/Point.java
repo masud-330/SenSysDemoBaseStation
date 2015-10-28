50350755b8772fdea23d24c15c54a87550f261b2
Muhammed <Muhammed@Mas-ud-pc>
2015-10-27 22:03:02 -0500

    GUI Code changes

--- src/org/sunspotworld/Point.java
+++ src/org/sunspotworld/Point.java
@@ -13,9 +13,32 @@ package org.sunspotworld;
 public class Point {
     public short x, y;
     
+    
+    // random point
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+    
     Point(short a, short b){
         x=a;
         y=b;
     }
     
+    // accessor methods
+    public double x() { return x; }
+    public double y() { return y; }
+    public double r() { return Math.sqrt(x*x + y*y); }
+
+    // Euclidean distance between this point and that point
+    public double distanceTo(Point that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+
+    // return a string representation of this point
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
